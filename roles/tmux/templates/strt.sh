d62b9310c495bbd8b1388acb7fd5384cb28a75ba
hannah98 <furiousgeorgecode@gmail.com>
2023-04-18 21:06:38 -0500

    smug

--- roles/tmux/templates/strt.sh
+++ roles/tmux/templates/strt.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
 session=$1
-
-if [ -d /mnt/user/persist/sessions ] && [ -e /mnt/user/persist/sessions/session.pl ]; then
-    if [ -z "$session" ]; then
-        session="main"
-    fi
-
-    if [ "$session" == "list" ]; then
-        unset session
-    fi
-    if [ "$session" == "dev" ]; then
-        echo 'Use alias "dev" instead of "strt"'
-        /mnt/user/persist/scripts/dev.sh
-        exit
-    fi
-    perl /mnt/user/persist/sessions/session.pl "$session"
-
-else
-    tmux new-session -d -s work -n main
-    tmux -2 attach-session -d -twork
+if [ -z "$session" ]; then
+    session="main"
 fi
+
+if [ "$session" == "list" ]; then
+    smug list
+    exit 0
+fi
+
+smug start $session || smug list
